27df6b7703c2e3003c71bc9235be7f1318c5f116
Mateusz Krolikowski <krolikmat@gmail.com>
2019-10-22 23:49:35 +0200

    Updates

---
+++ precache-manifest.753f677fe050f5184af82afe6f22c234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422fee4c764b33a7a3546140a4ab52b2",
+    "url": "/todo-react-hooks/index.html"
+  },
+  {
+    "revision": "3fd109f6caf517075dd5",
+    "url": "/todo-react-hooks/static/css/main.e186f168.chunk.css"
+  },
+  {
+    "revision": "21a99938ed45acd6f154",
+    "url": "/todo-react-hooks/static/js/2.58ece116.chunk.js"
+  },
+  {
+    "revision": "3fd109f6caf517075dd5",
+    "url": "/todo-react-hooks/static/js/main.bd92e1fe.chunk.js"
+  },
+  {
+    "revision": "dbed578768be4070ebd6",
+    "url": "/todo-react-hooks/static/js/runtime-main.0eefd568.js"
+  }
+]);
